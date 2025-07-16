2026620c457c6006be8d923bb7232e0ff79a443f
Snowy Renard <snowyrenard@gmail.com>
2025-07-16 23:09:19 +0200

    Update jellyfin

--- modules/jellyfin.nix
+++ modules/jellyfin.nix
@@ -5,6 +5,28 @@
 }: {
   services.tailscale.enable = true;
 
+  nixpkgs.overlays = with pkgs; [
+    (
+      final: prev:
+        {
+          jellyfin-web = prev.jellyfin-web.overrideAttrs (finalAttrs: previousAttrs: {
+            installPhase = ''
+              runHook preInstall
+
+              # this is the important line
+              sed -i "s#</head>#<script src=\"configurationpage?name=skip-intro-button.js\"></script></head>#" dist/index.html
+
+              mkdir -p $out/share
+              cp -a dist $out/share/jellyfin-web
+
+              runHook postInstall
+            '';
+          });
+        }
+    )
+  ];
+
+
   services.jellyfin = {
     enable = true;
     openFirewall = true;
