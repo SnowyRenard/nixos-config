3006ce2b8db9d066924e208e71894a719b345e45
Snowy Renard <snowyrenard@gmail.com>
2025-07-17 19:32:26 +0200

    Removed settings from browser

--- home/programs/browsers.nix
+++ home/programs/browsers.nix
@@ -5,50 +5,10 @@
   ...
 }: {
   programs = {
-    # chromium = {
-    #   enable = true;
-    # };
-
     firefox = {
       enable = true;
 
       profiles.${username} = {
-        bookmarks = {
-        force = true;
-          settings = [
-            {
-              name = "wikipedia";
-              tags = [ "wiki" ];
-              keyword = "wiki";
-              url = "https://en.wikipedia.org/wiki/Special:Search?search=%s&go=Go";
-            }
-            {
-              name = "kernel.org";
-              url = "https://www.kernel.org";
-            }
-            "separator"
-            {
-              name = "Nix sites";
-              toolbar = true;
-              bookmarks = [
-                {
-                  name = "homepage";
-                  url = "https://nixos.org/";
-                }
-                {
-                  name = "wiki";
-                  tags = [ "wiki" "nix" ];
-                  url = "https://wiki.nixos.org/";
-                }
-                {
-                  name = "home-manager";
-                  tags = [ "wiki" "nix" "home-manager" ];
-                  url = "https://nix-community.github.io/home-manager/";
-                }
-              ];
-            }
-          ];
-        };
       };
     };
   };
