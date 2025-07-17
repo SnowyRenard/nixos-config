a86142c10f0041f1268de800bcaf96b7e5d88b18
Snowy Renard <snowyrenard@gmail.com>
2025-07-17 13:45:25 +0200

    Added basics for laptop

---
+++ hosts/ASUS-A17/default.nix
@@ -0,0 +1,45 @@
+{
+  config,
+  pkgs,
+  ...
+}: {
+  imports = [
+    ../../modules/system.nix
+    ../../modules/lvm.nix
+    ../../modules/jellyfin.nix
+    ../../modules/hyprland.nix
+
+    ./hardware-configuration.nix
+  ];
+
+  # Bootloader.
+  boot.loader = {
+    systemd-boot.enable = true;
+    efi.canTouchEfiVariables = true;
+  };
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+  networking.hostName = "nixos-desktop";
+
+  # Nvidia Driver
+  services.xserver.videoDrivers = [ "nvidia" ];
+  hardware = {
+    graphics.enable = true;
+    nvidia = {
+      package = config.boot.kernelPackages.nvidiaPackages.stable;
+      modesetting.enable = true;
+
+      powerManagement = {
+        enable = true;
+        finegrained = false;
+      };
+
+      open =  true;
+
+      nvidiaSettings = false;
+    };
+  };
+
+  system.stateVersion = "25.05";
+}
