0a7704d942506bff7b259949bdb3cf0a45b42e88
Snowy Renard <snowyrenard@gmail.com>
2025-07-26 12:32:21 +0200

    Added basic search systems for firefox

--- home/programs/browsers.nix
+++ home/programs/browsers.nix
@@ -9,6 +9,61 @@
       enable = true;
 
       profiles.${username} = {
+        isDefault = true;
+
+        search.engines = {
+          nix-packages = {
+            name = "Nix Packages";
+            urls = [{
+              template = "https://search.nixos.org/packages";
+              params = [
+                { name = "type"; value = "packages"; }
+                { name = "query"; value = "{searchTerms}"; }
+              ];
+            }];
+
+            icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+            definedAliases = [ "@np" ];
+          };
+
+          nix-options = {
+            name = "Nix Options";
+            urls = [{
+              template = "https://search.nixos.org/options";
+              params = [
+                { name = "type"; value = "options"; }
+                { name = "query"; value = "{searchTerms}"; }
+              ];
+            }];
+
+            icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+            definedAliases = [ "@no" ];
+          };
+
+          youtube = {
+            name = "Youtube";
+            urls = [{
+              template = "https://youtube.com/results";
+              params = [
+                { name = "search_query"; value = "{searchTerms}"; }
+              ];
+            }];
+
+            definedAliases = [ "@yt" ];
+          };
+
+          wallhave= {
+            name = "Wallhaven";
+            urls = [{
+              template = "https://wallhaven.cc/search";
+              params = [
+                { name = "q"; value = "{searchTerms}"; }
+              ];
+            }];
+
+            definedAliases = [ "@wh" ];
+          };
+        };
       };
     };
   };
