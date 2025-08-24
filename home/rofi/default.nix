bc573ef7638425d2dda542ae318efcfb780f3629
Snowy Renard <snowyrenard@gmail.com>
2025-08-24 23:56:17 +0200

    Moved terminal into nix config for rofi

--- home/rofi/default.nix
+++ home/rofi/default.nix
@@ -1,6 +1,9 @@
 {
-  programs.rofi.enable = true;
-
+  programs.rofi = {
+    enable = true;
+    terminal = "\${pkgs.kitty}/bin/kitty --single-instance";
+  };
+  
   home.file.".config/rofi" = {
     source = ./configs;
     recursive = true;
