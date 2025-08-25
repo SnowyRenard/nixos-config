3af2af2209650148a04c8f5bffbf7061a8541e3f
Snowy Renard <snowyrenard@gmail.com>
2025-08-25 21:20:35 +0200

    Moved programs and added fcitx

--- users/snowyrenard/home.nix
+++ users/snowyrenard/home.nix
@@ -4,118 +4,15 @@
 
     ./editor.nix
     ./browser.nix
+    ./programs.nix
   ];
 
-    home.packages = with pkgs; [
-    # Archives
-    p7zip
-
-    # Monotoring
-    yazi
-    btop-rocm
-    nvtopPackages.full
-
-    # Utils
-    dust
-    tokei
-    ripgrep
-    pipe-rename
-    wiremix
-
-    # Programming
-    gcc
-    rustup
-
-    # Git
-    gitui
-
-    # Web Tools
-    curl
-    
-    # Games
-    discord
-    gamemode
-    xivlauncher
-    prismlauncher
-    path-of-building
-
-    # Productivity
-    krita
-    obsidian
-    # davinci-resolve
-
-    # Study
-    anki
-
-    # Media
-    jellyfin-media-player
-    ncspot
-  ];
-
-  home.sessionVariables = {
-    OBS_VKCAPTURE = 1;
-    FSR4_UPGRADE = 1; # proton-ge overwrite fsr3 to fsr4
-    PROTON_ENABLE_WAYLAND = 1;
-    PROTON_ENABLE_HDR = 1;
+  i18n.inputMethod = {
+    enable = true;
+    type = "fcitx5";
+    fcitx5.addons = with pkgs; [
+      fcitx5-mozc
+      fcitx5-gtk
+    ];
   };
-
-  programs = {
-    fastfetch = {
-      enable = true;
-      settings = {
-        modules = [
-          "title"
-          "separator"
-          "os"
-          "host"
-          "kernel"
-          "uptime"
-          "packages"
-          "shell"
-          "wm"
-          "cpu"
-          "gpu"
-          "memory"
-          "swap"
-          "disk"
-          "break"
-          "colors"
-        ];
-      };
-    };
-
-    mangohud = {
-      enable = true;
-      enableSessionWide = true;
-
-      settings = {
-        no_display = true;
-        preset = 3;
-        full = true;
-      };
-    };
-
-    git = {
-      enable = true;
-      userName = "Snowy Renard";
-      userEmail = "snowyrenard@gmail.com";
-    };
-
-    gh = {
-      enable = true;
-      gitCredentialHelper.enable = true;
-    };
-
-    obs-studio = {
-      enable = true;
-
-      plugins = with pkgs.obs-studio-plugins; [
-        wlrobs
-        obs-multi-rtmp
-        obs-pipewire-audio-capture
-        obs-vkcapture
-      ];
-    };
-  };
-
 }
---
+++ users/snowyrenard/programs.nix
@@ -0,0 +1,114 @@
+{ pkgs, ... }:
+{
+  home.packages = with pkgs; [
+    # Archives
+    p7zip
+
+    # Monotoring
+    yazi
+    btop-rocm
+    nvtopPackages.full
+
+    # Utils
+    dust
+    tokei
+    ripgrep
+    pipe-rename
+    wiremix
+
+    # Programming
+    gcc
+    rustup
+
+    # Git
+    gitui
+
+    # Web Tools
+    curl
+    
+    # Games
+    discord
+    gamemode
+    xivlauncher
+    prismlauncher
+    path-of-building
+
+    # Productivity
+    krita
+    obsidian
+    # davinci-resolve
+
+    # Study
+    anki
+
+    # Media
+    jellyfin-media-player
+    ncspot
+  ];
+
+  home.sessionVariables = {
+    OBS_VKCAPTURE = 1;
+    FSR4_UPGRADE = 1; # proton-ge overwrite fsr3 to fsr4
+    PROTON_ENABLE_WAYLAND = 1;
+    PROTON_ENABLE_HDR = 1;
+  };
+
+  programs = {
+    fastfetch = {
+      enable = true;
+      settings = {
+        modules = [
+          "title"
+          "separator"
+          "os"
+          "host"
+          "kernel"
+          "uptime"
+          "packages"
+          "shell"
+          "wm"
+          "cpu"
+          "gpu"
+          "memory"
+          "swap"
+          "disk"
+          "break"
+          "colors"
+        ];
+      };
+    };
+
+    mangohud = {
+      enable = true;
+      enableSessionWide = true;
+
+      settings = {
+        no_display = true;
+        preset = 3;
+        full = true;
+      };
+    };
+
+    git = {
+      enable = true;
+      userName = "Snowy Renard";
+      userEmail = "snowyrenard@gmail.com";
+    };
+
+    gh = {
+      enable = true;
+      gitCredentialHelper.enable = true;
+    };
+
+    obs-studio = {
+      enable = true;
+
+      plugins = with pkgs.obs-studio-plugins; [
+        wlrobs
+        obs-multi-rtmp
+        obs-pipewire-audio-capture
+        obs-vkcapture
+      ];
+    };
+  };
+}
