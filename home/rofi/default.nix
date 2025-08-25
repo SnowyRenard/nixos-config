aee34d606dde3ac0bd976161d5762de12017a53d
Snowy Renard <snowyrenard@gmail.com>
2025-08-25 11:19:30 +0200

    Moved some of the config files over

--- home/rofi/default.nix
+++ home/rofi/default.nix
@@ -1,7 +1,10 @@
+{ ... }:
 {
   programs.rofi = {
     enable = true;
+    font = "Fira Code Mono 10";
     terminal = "\${pkgs.kitty}/bin/kitty --single-instance";
+    theme = ./configs/config.rasi;
   };
   
   home.file.".config/rofi" = {
