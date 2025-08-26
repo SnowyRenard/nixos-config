dcf7da26247f936a56015d0b8701b8e1c259e97f
Snowy Renard <snowyrenard@gmail.com>
2025-08-27 00:39:45 +0200

    Added required deps for hypridle

--- home/hyprland/default.nix
+++ home/hyprland/default.nix
@@ -202,15 +202,15 @@
       listener = [
         {
             timeout = 150;                                # 2.5min.
-            on-timeout = "brightnessctl -s set 10";         # set monitor backlight to minimum, avoid 0 on OLED monitor.
-            on-resume = "brightnessctl -r";                 # monitor backlight restore.
+            on-timeout = "${pkgs.brightnessctl}/bin/brightnessctl -s set 10";         # set monitor backlight to minimum, avoid 0 on OLED monitor.
+            on-resume = "${pkgs.brightnessctl}/bin/brightnessctl -r";                 # monitor backlight restore.
         }
         
         # turn off keyboard backlight, comment out this section if you dont have a keyboard backlight.
         { 
             timeout = 150;                                          # 2.5min.
-            on-timeout = "brightnessctl -sd rgb:kbd_backlight set 0"; # turn off keyboard backlight.
-            on-resume = "brightnessctl -rd rgb:kbd_backlight";        # turn on keyboard backlight.
+            on-timeout = "${pkgs.brightnessctl}/bin/brightnessctl -sd rgb:kbd_backlight set 0"; # turn off keyboard backlight.
+            on-resume = "${pkgs.brightnessctl}/bin/brightnessctl -rd rgb:kbd_backlight";        # turn on keyboard backlight.
         }
         
         {
