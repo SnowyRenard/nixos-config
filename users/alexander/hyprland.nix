a07d12c8432de9b678629b27b4e1bc7fcfcf6308
Snowy Renard <snowyrenard@gmail.com>
2025-09-12 23:29:46 +0200

    updated alexander hdr settings

--- users/alexander/hyprland.nix
+++ users/alexander/hyprland.nix
@@ -9,26 +9,19 @@
       };
       # hdr
       experimental.xx_color_management_v4 = true;
-      debug.full_cm_proto = true;
       # monitor
       monitorv2 = [
         {
           output = "DP-1";
           mode = "highrr";
-          # bitdepth = 10;
-          # cm = "hdr";
-          # sdr_min_luminance = 0.005;
-          # sdr_max_luminance = 250;
+          bitdepth = 10;
           supports_wide_color = true;
           supports_hdr = true;
         }
         {
           output = "DP-9";
           mode = "highrr";
-          # bitdepth = 10;
-          # cm = "hdr";
-          # sdr_min_luminance = 0.005;
-          # sdr_max_luminance = 250;
+          bitdepth = 10;
           supports_wide_color = true;
           supports_hdr = true;
         }
