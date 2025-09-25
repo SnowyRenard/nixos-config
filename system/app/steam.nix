59870255476fe6a8692f95d5e6a280335e8821d9
Snowy Renard <snowyrenard@gmail.com>
2025-09-25 18:17:20 +0200

    fixed steam config

--- system/app/steam.nix
+++ system/app/steam.nix
@@ -1,7 +1,13 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   programs.steam = {
     enable = true;
+
+    remotePlay.openFirewall = true;
+    localNetworkGameTransfers.openFirewall = true;
+
+    extraPackages = with pkgs; [ gamemode ];
+    extraCompatPackages = with pkgs; [ proton-ge-bin ];
   };
 }
