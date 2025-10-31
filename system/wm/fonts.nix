2b5d17b62ff174d2b26695fcfa48267ef4d6cc08
Snowy Renard <snowyrenard@gmail.com>
2025-10-31 11:53:26 +0100

    fixed color fringing

--- system/wm/fonts.nix
+++ system/wm/fonts.nix
@@ -1,12 +1,16 @@
 { pkgs, ... }:
 
 {
-  fonts.packages = with pkgs; [
-      noto-fonts
-      noto-fonts-cjk-sans
-      noto-fonts-emoji
+  fonts = {
+    packages = with pkgs; [
+        noto-fonts
+        noto-fonts-cjk-sans
+        noto-fonts-emoji
 
-      fira-code
-      nerd-fonts.fira-code
-  ];
+        fira-code
+        nerd-fonts.fira-code
+    ];
+
+    fontconfig.subpixel.rgba = "vbgr";
+  };
 }
