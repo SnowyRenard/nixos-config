e2a1fc24a72862ee0508636cbfd22438e1dad550
Snowy Renard <snowyrenard@gmail.com>
2025-11-18 15:28:37 +0100

    nixos-desktop

---
+++ hosts/nixos-desktop/default.nix
@@ -0,0 +1,43 @@
+{ config, ... }: {
+  imports = [
+    ../../system/default.nix
+    ./hardware-configuration.nix
+    
+    ../../system/wm/hyprland.nix
+
+    ../../system/app/steam.nix
+    ../../system/app/gamemode.nix
+    ../../system/app/jellyfin.nix
+
+    ../../system/hardware/backlight.nix
+
+    ../../system/hardware/kernel.nix
+    ../../system/hardware/time.nix
+    
+    ../../system/hardware/mesa.nix
+
+    ../../system/security/firewall.nix
+    ../../system/security/tailscale.nix
+    ../../system/security/automount.nix
+    # ../../system/security/proton-vpn.nix
+  ];
+
+  # Bootloader.
+  boot.loader = {
+    systemd-boot.enable = true;
+    efi.canTouchEfiVariables = true;
+  };
+
+  # External monitor brightness control for hypridle.
+  boot.extraModulePackages = [config.boot.kernelPackages.ddcci-driver];
+  boot.kernelModules = ["i2c-dev" "ddcci_backlight"];
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+  networking.hostName = "nixos-laptop";
+
+  # Set your time zone.
+  time.timeZone = "Europe/Brussels";
+
+  system.stateVersion = "25.05";
+}
---
+++ hosts/nixos-desktop/partion.md
@@ -0,0 +1,12 @@
+**512 GB drive**
+---
+* /boot - 512MB
+* /nix - 128GB - noatime
+* / - 32GB
+* Remainder HDD cache
+
+**HDD**
+* /Vault - Full drive
+
+**2TB drive**
+* /home = Full drive
