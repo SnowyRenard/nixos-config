b7a3f985fa14c62ec8bcbdfda4c7b1d5c6bfcb1f
Snowy Renard <snowyrenard@gmail.com>
2025-12-03 22:38:18 +0100

    kernel update

--- system/hardware/kernel.nix
+++ system/hardware/kernel.nix
@@ -1,8 +1,9 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 
 {
   boot = {
     kernelPackages = pkgs.linuxPackages_latest;
+    extraModulePackages = with config.boot.kernelPackages; [];
     consoleLogLevel = 0;
     kernel.sysctl = {
       "vm.swappiness" = 10;
