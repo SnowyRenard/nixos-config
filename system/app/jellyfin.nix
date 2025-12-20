e836b33c99faf8041f03ad18e4b4b8fede670f0b
Snowy Renard <snowyrenard@gmail.com>
2025-12-20 21:24:49 +0100

    lock update and removed into skipper plugin overwrite

--- system/app/jellyfin.nix
+++ system/app/jellyfin.nix
@@ -4,29 +4,7 @@
   ...
 }: {
 
-  nixpkgs.overlays = [
-    (
-      final: prev:
-        {
-          jellyfin-web = prev.jellyfin-web.overrideAttrs (finalAttrs: previousAttrs: {
-            installPhase = ''
-              runHook preInstall
-
-              # this is the important line
-              sed -i "s#</head>#<script src=\"configurationpage?name=skip-intro-button.js\"></script></head>#" dist/index.html
-
-              mkdir -p $out/share
-              cp -a dist $out/share/jellyfin-web
-
-              runHook postInstall
-            '';
-          });
-        }
-    )
-  ];
-
-
-  services.jellyfin = {
+services.jellyfin = {
     enable = true;
     openFirewall = true;
     user = "${username}";
