3b3062e2143aece68acd8234877e14f652b59dd7
Snowy Renard <snowyrenard@gmail.com>
2026-01-28 10:33:00 +0100

    Moved some code from hyprland to ly

--- system/wm/hyprland.nix
+++ system/wm/hyprland.nix
@@ -6,8 +6,6 @@
     ./pipewire.nix
   ];
 
-  systemd.services.display-manager.environment.XDG_CURRENT_DESKTOP = "X-NIXOS-SYSTEMD-AWARE";
-
   programs.hyprland = {
     enable = true;
     withUWSM = true;
--- system/wm/wayland.nix
+++ system/wm/wayland.nix
@@ -7,5 +7,6 @@
   services.displayManager = {
     enable = true;
     ly.enable = true;
+    environment.XDG_CURRENT_DESKTOP = "X-NIXOS-SYSTEMD-AWARE";
   };  
 }
