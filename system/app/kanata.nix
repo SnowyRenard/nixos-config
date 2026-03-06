17c1634d2c4587003ad32b2020dc55e0723f2a2b
Snowy Renard <snowyrenard@gmail.com>
2026-03-06 18:50:36 +0100

    Reworked kanata

--- system/app/kanata.nix
+++ system/app/kanata.nix
@@ -1,39 +1,52 @@
 {
+  hardware.uinput.enable = true;
+
   services.kanata = {
     enable = true;
     keyboards = {
       default = {
         config = ''
           (defsrc
-            grv 1 2 3 4 5 6 7 8 9 0
-            q w e r t y u i o p [ ] \
-            a s d f g h j k l ; '
-            z x c v b n m , . /
-           )
-           (deflayer qwerty
-            @grl 1 2 3 4 5 6 7 8 9 0
-            q w e r t y u i o p [ ] \
-            a s d f g h j k l ; '
-            z x c v b n m , . /
-           )
-           (deflayer canary-angle
-            @grl 1 2 3 4 5 6 7 8 9 0
-            w l y p k z x o u ; [ ] \
-            c r s t b f n e i a '
-            j v d g q m h / , .
-           )
-           (defalias
+            grv  1 2 3 4 5 6 7 8 9 0 - = bspc
+            tab  q w e r t y u i o p [ ] \
+            caps a s d f g h j k l ; ' ret
+            lsft z x c v b n m , . / rsft
+            lctl lmet lalt spc ralt rmet rctl
+          )
+          (deflayer qwerty
+            @grl 1 2 3 4 5 6 7 8 9 0 - = bspc
+            tab  q w e r t y u i o p [ ] \
+            caps a s d f g h j k l ; ' ret
+            lsft z x c v b n m , . / rsft
+            lctl lmet lalt spc ralt rmet rctl
+          )
+          (deflayer canary-angle
+            @grl 1 2 3 4 5 6 7 8 9 0 - = bspc
+            tab  w l y p k z x o u ; [ ] \
+            caps c r s t b f n e i a ' ret
+            lsft j v d g q m h / , . rsft
+            lctl lmet lalt spc ralt rmet rctl
+          )
+          (deflayer command
+            _ _ _ _ _ _ _ _ _ _ _ _ _ _
+            _ _ _ _ _ _ _ _ _ _ _ _ _ _
+            _ _ _ _ _ _ _ _ _ _ _ _ _
+            _ _ _ _ _ _ _ _ _ _ _ _
+            _ _ _ _ _ _ _
+          )
+          (defalias
              grl (tap-hold 200 200 grv (layer-toggle layers))
            
              qwr (layer-switch qwerty)
              cnr (layer-switch canary-angle)
-           )
-           (deflayer layers
-            _ @qwr @cnr _ _ _ _ _ _ _ _
+          )
+          (deflayer layers
+            _ @qwr @cnr _ _ _ _ _ _ _ _ _ _ _
+            _ _ _ _ _ _ _ _ _ _ _ _ _ _
             _ _ _ _ _ _ _ _ _ _ _ _ _
-            _ _ _ _ _ _ _ _ _ _ _
-            _ _ _ _ _ _ _ _ _ _
-           )
+            _ _ _ _ _ _ _ _ _ _ _ _
+            _ _ _ _ _ _ _
+          )
         '';
         extraDefCfg = "process-unmapped-keys yes";
       };
